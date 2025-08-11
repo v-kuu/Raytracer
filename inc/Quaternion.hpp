84434f3456bc8c2ad15528f304ac081830f8c93c
Ville Kuusela <vkuusela@student.hive.fi>
2025-08-11 12:41:23 +0300

    Added quaternions for rotations

---
+++ inc/Quaternion.hpp
@@ -0,0 +1,23 @@
+#pragma once
+#include "Vec3.hpp"
+#include <cmath>
+
+struct	Quaternion
+{
+	float w;
+	float x;
+	float y;
+	float z;
+
+	Quaternion(void);
+	Quaternion(float w, float x, float y, float z);
+	Quaternion(Vec3 from, Vec3 to);
+
+	Quaternion	operator*(const Quaternion &other) const;
+	bool		operator==(const Quaternion &other) const;
+
+	Vec3		rotate(const Vec3 &vec) const;
+	float		length(void) const;
+	Quaternion	normalize(void) const;
+	Quaternion	inverse(void) const;
+};
---
+++ src/Quaternion.cpp
@@ -0,0 +1,72 @@
+#include "../inc/Quaternion.hpp"
+
+Quaternion::Quaternion(void) : w(1), x(0), y(0), z(0)
+{
+}
+
+Quaternion::Quaternion(float w, float x, float y, float z)
+	: w(w), x(x), y(y), z(z)
+{
+}
+
+Quaternion::Quaternion(Vec3 from, Vec3 to)
+{
+	float dot_p = dot(from, to);
+	if (dot_p < 1.0f)
+	{
+		Vec3 axis = cross(from, to);
+		float angle = acosf(dot_p);
+		w = cos(angle / 2);
+		x = axis.x * sin(angle / 2);
+		y = axis.y * sin(angle / 2);
+		z = axis.z * sin(angle / 2);
+	}
+	else
+	{
+		w = 1.0f;
+		x = 0;
+		y = 0;
+		z = 0;
+	}
+}
+
+Quaternion	Quaternion::operator*(const Quaternion &other) const
+{
+	Vec3 first(x, y, z);
+	Vec3 second(other.x, other.y, other.z);
+	float scalar = w * other.w - dot(first, second);
+	Vec3 vector = second * w + first * other.w + cross(first, second);
+	return (Quaternion(scalar, vector.x, vector.y, vector.z));
+}
+
+bool	Quaternion::operator==(const Quaternion &other) const
+{
+	if (w == other.w && x == other.x && y == other.y && z == other.z)
+		return (true);
+	return (false);
+}
+
+Vec3	Quaternion::rotate(const Vec3 &other) const
+{
+	Vec3 vector(x, y, z);
+	Vec3 ret = (vector * 2.0f * dot(vector, other)
+			+ (other * (w * w - dot(vector, vector)))
+			+ (cross(vector, other) * w * 2.0f));
+	return (ret);
+}
+
+float	Quaternion::length(void) const
+{
+	return (sqrtf(w * w + x * x + y * y + z * z));
+}
+
+Quaternion	Quaternion::normalize(void) const
+{
+	float len = this->length();
+	return (Quaternion(w / len, x / len, y / len, z / len));
+}
+
+Quaternion	Quaternion::inverse(void) const
+{
+	return (Quaternion(w, -x , -y, -z));
+}
